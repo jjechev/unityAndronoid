fdc0d767c36fdadbf1cf070ce37b024a57623e0d
Dobromir <dobromircho79@gmail.com>
2016-06-02 22:31:46 +0300

    test

--- Assets/scripts/BallScript.cs
+++ Assets/scripts/BallScript.cs
@@ -64,55 +64,55 @@ public class BallScript : MonoBehaviour
     {
         sound.Play();
 
-        if (col.collider.tag == "player")
-        {
-            speedX = -((col.transform.position.x - this.transform.position.x) / col.transform.localScale.x / 5);
-            speedY = -speedY;
-        }
+       //if (col.collider.tag == "player")
+       //{
+       //    speedX = -((col.transform.position.x - this.transform.position.x) / col.transform.localScale.x / 5);
+       //    speedY = -speedY;
+       //}
 
-        if (col.collider.tag == "wallUp")
-        {
-            speedY = -speedY;
-        }
+       //if (col.collider.tag == "wallUp")
+       //{
+       //    speedY = -speedY;
+       //}
 
-        if (col.collider.tag == "wallDown")
-        {
-            speedY = -speedY;
-            GameManagerScript.instance.LostLife();
-        }
+       //if (col.collider.tag == "wallDown")
+       //{
+       //    speedY = -speedY;
+       //    GameManagerScript.instance.LostLife();
+       //}
 
-        if (col.collider.tag == "wallLeft" || col.collider.tag == "wallRight")
-        {
-            speedX = -speedX;
-        }
+       //if (col.collider.tag == "wallLeft" || col.collider.tag == "wallRight")
+       //{
+       //    speedX = -speedX;
+       //}
 
-        if (col.collider.tag == "brick")
-        {
-            float brickMaxHeight = col.transform.localScale.y;
-            float brickMaxWidth = col.transform.localScale.x;
+       //if (col.collider.tag == "brick")
+       //{
+       //    float brickMaxHeight = col.transform.localScale.y;
+       //    float brickMaxWidth = col.transform.localScale.x;
 
-            float absoluteBallX = this.transform.position.x;
-            float absoluteBallY = this.transform.position.y;
+       //    float absoluteBallX = this.transform.position.x;
+       //    float absoluteBallY = this.transform.position.y;
 
-            float absoluteBrickX = col.transform.position.x;
-            float absoluteBrickY = col.transform.position.y;
+       //    float absoluteBrickX = col.transform.position.x;
+       //    float absoluteBrickY = col.transform.position.y;
 
-            if (
-                    absoluteBrickX + brickMaxWidth / 2 < absoluteBallX ||
-                    absoluteBrickX - brickMaxWidth / 2 > absoluteBallX
-                )
-            {
-                speedX = -speedX;
-            }
+       //    if (
+       //            absoluteBrickX + brickMaxWidth / 2 < absoluteBallX ||
+       //            absoluteBrickX - brickMaxWidth / 2 > absoluteBallX
+       //        )
+       //    {
+       //        speedX = -speedX;
+       //    }
 
-            if (
-                    absoluteBrickY + brickMaxHeight / 2 < absoluteBallY ||
-                    absoluteBrickY - brickMaxHeight / 2 > absoluteBallY
-                )
-            {
-                speedY = -speedY;
-            }
-        }
+       //    if (
+       //            absoluteBrickY + brickMaxHeight / 2 < absoluteBallY ||
+       //            absoluteBrickY - brickMaxHeight / 2 > absoluteBallY
+       //        )
+       //    {
+       //        speedY = -speedY;
+       //    }
+       //}
     }
 
 }
